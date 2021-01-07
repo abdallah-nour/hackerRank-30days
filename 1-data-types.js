08b81d6fc5b5939562797c3b2b2ca377dc4e3737
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-01-07 08:17:31 +0200

    #1
    data-types

---
+++ 1-data-types.js
@@ -0,0 +1,7 @@
+function main(integer, double, string) {
+  console.log(integer + 4);
+  console.log((double + 4).toFixed(1));
+  console.log("HackerRank " + string);
+}
+
+main(20, 40, "here is a string");
